2e9cd1d1d1948c44c7f11755ee5e6a75f359b8e5
Félix Galindo <galindo.fe@gmail.com>
2019-06-13 18:48:19 -0400

    layout, header y react-router-dom

--- src/components/clientes/Clientes.js
+++ src/components/clientes/Clientes.js
@@ -1,6 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import clienteAxios from '../../config/axios';
 
 export const Clientes = props => {
+  const consultarApi = async () => {
+    const clientes = await clienteAxios.get('/clientes');
+    console.log(clientes);
+  };
+  //useEffect es similar a componentDidMount
+  useEffect(() => {
+    consultarApi();
+  });
   return <h2>Clientes</h2>;
 };
 
--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,14 +1,11 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 
 const Header = () => (
-  //Fragment se usa para envolver elementos ya que el componente debe tener un padre obligatoriamente
-  <Fragment>
-    <header className="barra">
-      <div className="contenedor">
-        <h1>CRM - Administrador de Clientes</h1>
-      </div>
-    </header>
-  </Fragment>
+  <header className="barra">
+    <div className="contenedor">
+      <h1>CRM - Administrador de Clientes</h1>
+    </div>
+  </header>
 );
 
 export default Header;
