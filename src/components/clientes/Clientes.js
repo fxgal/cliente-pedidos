3bc52829604afa093f81e42c39c9b03f3eefbeeb
Félix Galindo <galindo.fe@gmail.com>
2019-06-15 11:13:27 -0400

    mostrando datos de clientes

--- src/components/clientes/Clientes.js
+++ src/components/clientes/Clientes.js
@@ -1,16 +1,33 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState, Fragment } from 'react';
+
+import Cliente from './Cliente';
+
 import clienteAxios from '../../config/axios';
 
 export const Clientes = props => {
+  const [clientes, setClientes] = useState([]);
+
   const consultarApi = async () => {
-    const clientes = await clienteAxios.get('/clientes');
-    console.log(clientes);
+    const clientesQuery = await clienteAxios.get('/clientes');
+    setClientes(clientesQuery.data.clientes);
   };
   //useEffect es similar a componentDidMount
   useEffect(() => {
     consultarApi();
-  });
-  return <h2>Clientes</h2>;
+  }, []);
+  return (
+    <Fragment>
+      <h2>Clientes</h2>
+      <a href="nuevo-cliente.html" className="btn btn-verde nvo-cliente">
+        <i className="fas fa-plus-circle"></i>
+        Nuevo Cliente
+      </a>
+      {clientes.map(cliente => (
+        <Cliente key={cliente._id} cliente={cliente} />
+      ))}
+      <ul className="listado-clientes"></ul>
+    </Fragment>
+  );
 };
 
 export default Clientes;
