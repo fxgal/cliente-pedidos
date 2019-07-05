9f06c278773d0141c1f0508f5782649e5903cd7e
Félix Galindo <galindo.fe@gmail.com>
2019-07-05 17:41:28 -0400

    lista de pedidos

--- src/components/pedidos/Pedido.js
+++ src/components/pedidos/Pedido.js
@@ -1,46 +1,42 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
-const Pedido = props => {
+const Pedido = ({ pedido }) => {
   return (
-    <li className="pedido">
-      <div className="info-pedido">
-        <p className="id">ID: 0192019201291201</p>
-        <p className="nombre">Cliente: Juan Pablo De la torre</p>
+    <Fragment>
+      <li className="pedido">
+        <div className="info-pedido">
+          <p className="id">ID: {pedido.cliente._id}</p>
+          <p className="nombre">
+            Cliente: {pedido.cliente.nombre} {pedido.cliente.apellido}
+          </p>
 
-        <div className="articulos-pedido">
-          <p className="productos">Artículos Pedido: </p>
-          <ul>
-            <li>
-              <p>Macbook Pro</p>
-              <p>Precio: $3000</p>
-              <p>Cantidad: 4</p>
-            </li>
-            <li>
-              <p>Macbook Pro</p>
-              <p>Precio: $3000</p>
-              <p>Cantidad: 4</p>
-            </li>
-            <li>
-              <p>Macbook Pro</p>
-              <p>Precio: $3000</p>
-              <p>Cantidad: 4</p>
-            </li>
-          </ul>
+          <div className="articulos-pedido">
+            <p className="productos">Artículos Pedido: </p>
+            <ul>
+              {pedido.pedido.map((ped, index) => (
+                <li key={ped._id}>
+                  <p>{ped.nombre}</p>
+                  <p>Precio: ${ped.precio}</p>
+                  <p>Cantidad: {ped.cantidad}</p>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <p className="total">Total: ${pedido.total} </p>
         </div>
-        <p className="total">Total: $3,500 </p>
-      </div>
-      <div className="acciones">
-        <a href="#" className="btn btn-azul">
-          <i className="fas fa-pen-alt"></i>
-          Editar Pedido
-        </a>
+        <div className="acciones">
+          <a href="#" className="btn btn-azul">
+            <i className="fas fa-pen-alt"></i>
+            Editar Pedido
+          </a>
 
-        <button type="button" className="btn btn-rojo btn-eliminar">
-          <i className="fas fa-times"></i>
-          Eliminar Pedido
-        </button>
-      </div>
-    </li>
+          <button type="button" className="btn btn-rojo btn-eliminar">
+            <i className="fas fa-times"></i>
+            Eliminar Pedido
+          </button>
+        </div>
+      </li>
+    </Fragment>
   );
 };
 
--- src/components/pedidos/Pedidos.js
+++ src/components/pedidos/Pedidos.js
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Pedido from './Pedido';
+import conectionAxios from '../../config/axios';
+import { withRouter } from 'react-router-dom';
 
 export const Pedidos = props => {
+  const [pedidos, setPedidos] = useState([]);
+
+  const consultarPedidos = async () => {
+    const pedidosQuery = await conectionAxios.get('/pedidos');
+    setPedidos(pedidosQuery.data.pedidos);
+  };
+
+  useEffect(() => {
+    consultarPedidos();
+  }, []);
+
   return (
     <main className="caja-contenido col-9">
       <h2>Pedidos</h2>
       <ul className="listado-pedidos">
-        <Pedido />
+        {pedidos.map((pedido, index) => (
+          <Pedido key={pedido._id} pedido={pedido} />
+        ))}
       </ul>
     </main>
   );
